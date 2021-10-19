4d199052545c1a3a9035b37b33c1ea9cfc8f5e1c
unknown <yourcodepartner@gmail.com>
2021-10-19 13:51:27 +0530

    name changed

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -29,8 +29,8 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
 [f])}}}var i=location.href.replace(/#.*/,""),c=b.localScroll=function(a){b("body").localScroll(a)};c.defaults={duration:1E3,axis:"y",event:"click",stop:!0,target:window,reset:!0};c.hash=function(a){if(location.hash){a=b.extend({},c.defaults,a);a.hash=!1;if(a.reset){var e=a.duration;delete a.duration;b(a.target).scrollTo(0,a);a.duration=e}g(0,location,a)}};b.fn.localScroll=function(a){function e(){return!!this.href&&!!this.hash&&this.href.replace(this.hash,"")==i&&(!a.filter||b(this).is(a.filter))}
 a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b([d.target,d.target.parentNode]).filter(e)[0];c&&g(d,c,a)}):this.find("a,area").filter(e).bind(a.event,function(b){g(b,this,a)}).end().end()}})(jQuery);
 
-$(function(){
-    $('#smoothScroll btn btn-default')
-};
+// $(function(){
+//     $('#smoothScroll btn btn-default')
+// };
 // Initialize all .smoothScroll links
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
